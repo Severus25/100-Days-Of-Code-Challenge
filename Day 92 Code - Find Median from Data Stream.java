d04cd4fe753e60db2876419b521951895357fbd0
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-08-05 00:27:18 +0530

    Create Day 92 Code - Find Median from Data Stream.java

---
+++ Day 92 Code - Find Median from Data Stream.java
@@ -0,0 +1,22 @@
+// Problem Statement:
+// https://leetcode.com/problems/find-median-from-data-stream/
+
+// Problem Solution:
+class MedianFinder {
+
+    private Queue<Long> small = new PriorityQueue(),
+                        large = new PriorityQueue();
+
+    public void addNum(int num) {
+        large.add((long) num);
+        small.add(-large.poll());
+        if (large.size() < small.size())
+            large.add(-small.poll());
+    }
+
+    public double findMedian() {
+        return large.size() > small.size()
+               ? large.peek()
+               : (large.peek() - small.peek()) / 2.0;
+    }
+};
